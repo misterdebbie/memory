db287bda17c2be652eda886ad8ed34f8517eb876
mr.debbie <giyoungchong1980@gmail.com>
2017-11-04 07:57:03 -0400

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,8 @@
+//console.log("Up and running!");
+var cardOne = "queen";
+var cardOneTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipper " + cardOne);
+console.log("User flipper " + cardFour);
