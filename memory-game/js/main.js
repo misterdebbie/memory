b33e2069133e07a7543d14d4c9a9d09495192ced
mr.debbie <giyoungchong1980@gmail.com>
2017-11-10 10:04:33 -0500

    add js

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,35 @@
 //console.log("Up and running!");
-var cardOne = "queen";
-var cardOneTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipper " + cardOne);
-console.log("User flipper " + cardFour);
+var cards = [
+  {rank:"queen",suit:"hearts",cardImage:"images/queen-of-hearts.png"},
+  {rank:"queen",suit:"diamonds",cardImage:"images/queen-of-diamonds.png"},
+  {rank:"king",suit:"hearts",cardImage:"images/king-of-hearts.png"},
+  {rank:"king",suit:"diamonds",cardImage:"images/king-of-diamonds.png"}
+];
+
+var cardsInPlay =[];
+var checkForMatch = function (){
+    if (cardsInPlay[0]===cardsInPlay[1]){
+      console.log("You found a match!");
+    } else {
+      console.log("Sorry, try again.");
+    }
+}
+
+var flipCard = function (cardId){
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].suit);
+  console.log("User flipped " + cards[cardId].cardImage);
+
+  checkForMatch();
+}
+
+var createBoard = function (){
+  for (var i = 0;i < theArray.length;i++){
+    var newImg = document.createElement('img');
+    newImg.setAttribute('src','images/back.png');
+  }
+}
+flipCard(0);
+flipCard(2);
